781b08084a6b074c9752591a946be0156edba25c
shahram moradpour <51191070+strumer69@users.noreply.github.com>
2024-05-01 17:56:25 +0200

    simple program in C++ VHB course

---
+++ 1_exercise.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+# include <cstdlib>
+using namespace std;
+int main() {
+        cout << " this is my address" << endl;
+        cout <<  " this is my phone number : 12345678" << endl;
+system ("pause");
+}
