0aef64493473dbcf2d300f198bf5180ae7d57285
shahram <shmoradpour69@gmail.com>
2024-05-09 08:37:13 +0200

    if statement learning

---
+++ 2_if_statement.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cstdlib>
+
+int main(){
+int x;
+std::cout << "please enter a number:";
+std::cin >> x;
+if (x>10){
+    std::cout << " number is big" << std::endl;
+    }
+    return 0;
+}
