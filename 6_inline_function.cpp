4ade245b37c81d04b8594e706240e53eb4b9cca1
shahram <shmoradpour69@gmail.com>
2024-05-18 14:04:01 +0200

    inline function

---
+++ 6_inline_function.cpp
@@ -0,0 +1,10 @@
+//Inline function
+// without writing a function, write a code to print the max or min of two givven number
+#include <iostream>
+using namespace std;
+
+int main(){
+cout << "min of 2,5 is: "<< (2>5 ? 5:2) << endl;
+cout << "max 3,8 is : "<< (3>8 ? 3:8) << endl;
+return 0 ;
+}
